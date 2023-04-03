086c0e05e23572a66cc17c5a31f76e1f4a3b720e
sameermd16 <sameerthemc@gmail.com>
2023-04-03 20:06:16 +0530

    menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import CloseIcon from '@material-ui/icons/Close';
+import CloseIcon from '@material-ui/icons/Close'
+
 
 function Header() {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <Container>
       
-        <a href='#'>
-          <img src='/images-2/logo.svg'/>
-        </a>
+      <a href='#'>
+        <img src='/images-2/logo.svg'/>
+      </a>
       
       <Menu>
         <p><a href='#'>Model S</a></p>
@@ -19,8 +23,8 @@ function Header() {
         <p><a href='#'>Powerwall</a></p>
         <p><a href='#'>Shop</a></p>
         <p><a href='#'>Account</a></p>
-        <p><a href='#' id='menuBtn'>Menu</a></p>
       </Menu>
+        <p><a href='#' id='menuBtn'>Menu</a></p>
 
       <Section id='mobileMenu'>
         <IconClose id='closeBtn'></IconClose>
@@ -56,6 +60,7 @@ const Container = styled.div`
   align-items: center;
   padding: 20px 30px 0px 60px;
   width: 100%;
+  z-index: 1;
 `;
 
 
@@ -75,17 +80,24 @@ const Menu = styled.div`
     transition: 0.3s ease-in-out;
     transform-origin: left;
   }
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
-const Section = styled.div`
-  position: absolute;
-  right: 0;
+const Section = styled.div`     //mobileMenu
+  position: fixed;
+  right: -300px;
   top: 0;
+  bottom: 0;
   height: 100vh;
   width: 300px;
   background-color: #f2f2f2;
   padding: 30px;
   display: none;
+  z-index: 10;
+  transition: all 0.2s ease-in;
+  // transform: ${props => props.show ? 'translateX(0)': 'translateX(100%)'}
   p {
     margin-bottom: 10px;
     padding: 5px 10px;
@@ -95,6 +107,12 @@ const Section = styled.div`
   p:hover {
     background: lightgrey;
     cursor: pointer;
+  }
+  @media (max-width: 768px) {
+    width: 80vw;
+    z-index: 99;
+    
+    
   }
 `;
 
@@ -115,11 +133,18 @@ const menuBtn = document.getElementById('menuBtn');
 const mobileMenu = document.getElementById('mobileMenu');
 const closeBtn = document.getElementById('closeBtn');
 
-
 menuBtn.addEventListener('click', () => {
-  mobileMenu.style.display = 'block';
+  mobileMenu.style.right = 0
+})
+closeBtn.addEventListener('click', () => {
+  mobileMenu.style.right = '-300px'
 })
 
-closeBtn.addEventListener('click', () => {
-  mobileMenu.style.display = 'none';
-})
+
+// menuBtn.addEventListener('click', () => {
+//   mobileMenu.style.display = 'block';
+// })
+
+// closeBtn.addEventListener('click', () => {
+//   mobileMenu.style.display = 'none';
+// })
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal/Fade'
 
 function Section({title, description, leftBtnText, rightBtnText, backgroundImg}) {
   return (
     <Wrap bgImage={backgroundImg}>
+      <Fade bottom>
         <ItemText>
           <h1>{title}</h1>
           <p>{description}</p>
         </ItemText>
+      </Fade>
         <Buttons>
           <ButtonGroup>
             <LeftButton> {leftBtnText} </LeftButton>
@@ -52,6 +55,7 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
   display: flex;
   margin-bottom: 20px;
+  
   @media (max-width: 768px) {
     flex-direction: column;
     margin-bottom: 10px;
